bbabd959e0c0a04b469381745fbcc50ee570ca87
Felipe <rdsantos.felipe@gmail.com>
2025-08-28 23:13:00 -0300

    Adicionei a soma e o seu teste

---
+++ src/calculadora.js
@@ -0,0 +1,35 @@
+function somarDoisNumeros(valor1, valor2){
+    const resultado = valor1 + valor2;
+    return resultado;
+
+}
+
+function calcularMediaDeDoisNumeros(valor1, valor2){
+    //1. Soma todos os valores
+    const resultadoSomaDeDoisValores = somarDoisNumeros(valor1, valor2);
+    //2. Divide pela quantidade de números passados
+    
+    const mediaDeDoisValores = resultadoSomaDeDoisValores / 2;
+    
+    //3. Retornar o resultado
+    return mediaDeDoisValores;
+
+}
+
+
+function somarCamposDaTela(){
+    const valor1 = n1;
+    const valor2 = n2;
+
+    const resultado = somarDoisNumeros(valor1,valor2);
+    console.log(resultado);
+
+}
+
+module.exports = {
+    somarDoisNumeros
+},
+
+module.exports = {
+    calcularMediaDeDoisNumeros
+}
---
+++ tests/calculadora.test.js
@@ -0,0 +1,7 @@
+const {somarDoisNumeros} = require('../src/calculadora');
+
+
+const resultadoDaSoma = somarDoisNumeros(5,3);
+
+console.log('O resultado da soma dos dois números é: ', resultadoDaSoma)
+
